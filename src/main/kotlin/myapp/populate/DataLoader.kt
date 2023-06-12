ed474d27678bb4f257221005fbc0be51c82a5071
Matis Karamenderes <matis.kara@gmail.com>
2023-06-12 11:41:56 +0200

    dataloader

--- src/main/kotlin/myapp/populate/DataLoader.kt
+++ src/main/kotlin/myapp/populate/DataLoader.kt
@@ -1,2 +1,18 @@
 package myapp.populate
 
+import jakarta.annotation.PostConstruct
+import myapp.model.Movie
+import myapp.model.User
+import myapp.service.MovieService
+import myapp.service.UserService
+import org.springframework.stereotype.Component
+
+@Component
+class DataLoader(val userService: UserService, val movieService: MovieService) {
+
+    @PostConstruct
+    fun loadData() {
+        userService.createUser(User(username = "toto", password = "toto"))
+        movieService.save(Movie(name = "My Favorite Movie"))
+    }
+}
